f623b9b7e285a7c1923398502b02ab815260c4b9
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-08-15 16:15:02 -0300

    3 Define o método construtor no CpuService e adiciona PowerService a ele

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from '../power/power.service';
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  // 3 chame o powerService 
+  constructor(private powerService: PowerService) { }
+
+  // apenas um exemplo simples para utilizar o powerService
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from Power Service');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
+}
