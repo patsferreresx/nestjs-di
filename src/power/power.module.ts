34c1cb516ecb89a1ba58f91e3191421ee17d6554
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-08-15 16:13:49 -0300

    1 Add PowerService na lista de exports do PowerModule

--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService],
+  exports: [PowerService] // 1 exporte para outro modulo ter acesso
 })
 export class PowerModule {}
