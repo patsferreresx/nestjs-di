9d5c79ae467142a59d2bb951793759e3ebf0fecf
Patricia Ferrer <patricia.ferreira@esx.com.br>
2024-08-15 16:14:25 -0300

    2 Import o PowerModule dentro da CpuModule

--- src/cpu/cpu.module.ts
+++ src/cpu/cpu.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CpuService } from './cpu.service';
+import { PowerModule } from '../power/power.module';
 
 @Module({
+  imports: [PowerModule], // 2 importa o powermodule
   providers: [CpuService]
 })
 export class CpuModule {}
